19aee53b31c4d907376594b323714cdf048b5f2a
bingguxi <kongsuni3685@gmail.com>
2023-06-05 16:45:49 +0900

    Initial commit

--- src/main/java/kopo/poly/AiJavaPrjApplication.java
+++ src/main/java/kopo/poly/AiJavaPrjApplication.java
@@ -1,13 +1,17 @@
 package kopo.poly;
 
 
+import kopo.poly.dto.NlpDTO;
 import kopo.poly.dto.OcrDTO;
+import kopo.poly.service.INlpService;
 import kopo.poly.service.IOcrService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,6 +22,8 @@ public class AiJavaPrjApplication implements CommandLineRunner {
     // Spring Frameworks 실행될 때, @Service 정의한 자바는 자동으로 메모리에 올림
     // 메모리에 올라간 OcrService 객체를 ocrService 변수에 객체를 넣어주기
     private final IOcrService ocrService; // 이미지 인식
+
+    private final INlpService nlpService; //  자연어 처리
 
     public static void main(String[] args) {
 
@@ -30,8 +36,8 @@ public class AiJavaPrjApplication implements CommandLineRunner {
         log.info("자바 프로그래밍 시작!!");
 
         String filePath = "image"; // 문자열을 인식할 이미지 파일 경로
-        String fileName = "sample02.png"; // 문자열을 인식할 이미지 파일 이름
-        // String fileName = "sample01.jpg
+        String fileName = "sample01.jpg"; // 문자열을 인식할 이미지 파일 이름
+        // String fileName = "sample02.png
 
         // 전달할 값 (Parameter) 약자로 보통 변수명 앞에 p를 붙임 => pDTO
         OcrDTO pDTO = new OcrDTO(); // OcrService의 함수에 정보를 전달할 DTO를 메모리에 올리기
@@ -46,6 +52,34 @@ public class AiJavaPrjApplication implements CommandLineRunner {
 
         log.info("인식된 문자열");
         log.info(result);
+
+        log.info("------------------------------------------------------------------");
+        NlpDTO nlpDTO = nlpService.getPlainText(result);
+        log.info("형태소별 품사 분석 결과 : " + nlpDTO.getResult());
+
+        nlpDTO = nlpService.getNouns(result);
+
+        List<String> nouns = nlpDTO.getNouns();
+
+        Set<String> distinct = new HashSet<>(nouns);
+
+        log.info("중복 제거 수행 전 단어 수 : " + nouns.size());
+        log.info("중복 제거 수행 후 단어 수 : " + distinct.size());
+
+        Map<String, Integer> rMap = new HashMap<>();
+
+        for (String s : distinct) {
+            int count = Collections.frequency(nouns, s);
+            rMap.put(s, count);
+
+            log.info(s + " : " + count);
+        }
+
+        List<Map.Entry<String, Integer>> sortResult = new LinkedList<>(rMap.entrySet());
+
+        Collections.sort(sortResult, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        log.info("가장 많이 사용된 단어는? : " + sortResult);
 
         log.info("자바 프로그래밍 종료!!");
     }
