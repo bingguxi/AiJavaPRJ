8680afe0880fdbafaa0c57b2f2a49b62b4e7331b
bingguxi <kongsuni3685@gmail.com>
2023-12-14 09:26:41 +0900

    initial commit

--- src/main/java/kopo/poly/dto/OcrDTO.java
+++ src/main/java/kopo/poly/dto/OcrDTO.java
@@ -10,3 +10,28 @@ public class OcrDTO {
     private String fileName;
     private String result;
 }
+//    만약 @Getter, @Setter 를 사용하지 않는다면?
+
+    /*public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }*/
